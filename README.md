9be6ea24d2ec873e08848bf4ed3060a8e61f42a7
Arash Rahmani <arashr693@gmail.com>
2026-06-17 19:43:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# LeetCode Solutions
+
+Solutions in Python.
+
+| # | Problem | Difficulty | Solution |
+|---|---------|------------|----------|
+| 2769 | Find the Maximum Achievable Number | Easy | [Python](easy/2769-find-the-maximum-achievable-number.py) |
---
+++ Solutions/2769-find-the-maximum-achievable-number.py
@@ -0,0 +1,8 @@
+# 2769. Find the Maximum Achievable Number
+# https://leetcode.com/problems/find-the-maximum-achievable-number/
+# Difficulty: Easy
+# Time: O(1) | Space: O(1)
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2 * t
